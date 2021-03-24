916f1affc12d96b3bd771fc52c5c63b94def6052
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-24 20:31:59 -0300

    Terminado o exercício

---
+++ bloco_4/dia_1/segundo-exercicio/scripts.js
@@ -0,0 +1,25 @@
+/*
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail, typeof patientAge);
+
+patientId = '50'
+
+console.log(typeof patientId);
+
+*/
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base * 4
+
+console.log(area);
+console.log(perimetro)
