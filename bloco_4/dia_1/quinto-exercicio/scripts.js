420d0385573934e7cbdcb0193dc7df09080886f9
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-25 12:37:10 -0300

    Parte 2 terminado

--- bloco_4/dia_1/quinto-exercicio/scripts.js
+++ bloco_4/dia_1/quinto-exercicio/scripts.js
@@ -1,5 +1,15 @@
 let a = 10;
 let b = 2;
+let maior;
 
 console.log('Adição:', a + b, '\nSubtração:', a - b, '\nMultiplicação:', a * b, '\nDivisão:', a / b, '\nMódulo:', a % b);
+
+if (a > b) {
+  maior = a;
+  console.log('\nO valor', a, 'é maior que o valor', b)
+}
+else {
+  maior = b;
+  console.log('\nO valor', b, 'é maior que o valor', a)
+}
 
