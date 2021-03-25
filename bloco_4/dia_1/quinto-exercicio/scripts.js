4e8d3e1087e8030d93edd4b9fd056097469f7941
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-25 15:24:44 -0300

    Parte 6 terminado, e melhorias no código

--- bloco_4/dia_1/quinto-exercicio/scripts.js
+++ bloco_4/dia_1/quinto-exercicio/scripts.js
@@ -1,15 +1,13 @@
+// Parte 1
+
 let a = 10;
 let b = 2;
 let c = 1;
-let degreeAngleA;
-let degreeAngleB;
-let degreeAngleC;
-let total;
-let isPositive;
 
 console.log('Adição:', a + b, '\nSubtração:', a - b, '\nMultiplicação:', a * b, '\nDivisão:', a / b, '\nMódulo:', a % b);
 
 
+// Parte 2
 
 
 if (a > b) {
@@ -19,6 +17,8 @@ else {
   console.log('\nO valor', b, 'é maior que o valor', a)
 };
 
+
+// Parte 3
 
 
 a = 7
@@ -40,7 +40,7 @@ else {
 
 
 
-
+// Parte 4
 
 
 a = 0;
@@ -56,12 +56,14 @@ else {
 };
 
 
+// Parte 5
 
 
-
-degreeAngleA = 30;
-degreeAngleB = 60;
-degreeAngleC = 90;
+let degreeAngleA = 30;
+let degreeAngleB = 60;
+let degreeAngleC = 90;
+let total;
+let isPositive;
 
 total = degreeAngleA + degreeAngleB + degreeAngleC;
 
@@ -77,4 +79,40 @@ if (isPositive) {
 }
 else {
   console.log('Um valor negativo foi detectado');
+}
+
+
+// Parte 6
+
+
+let nameChessPieces = 'rei';
+
+switch (nameChessPieces.toLowerCase()) {
+  case 'peão':
+    console.log('O peão movimenta 1 casa pra frente, no primeiro movimento ele pode movimentar 2 casas, e come peça na diagonal')
+    break;
+
+  case 'torre':
+    console.log('A torre movimenta na horizontal e vertical')
+    break;
+
+  case 'cavalo':
+    console.log('O cavalo movimenta em formato de "L", ou seja movimenta 3 casas e depois vira 2 casas')
+    break;
+
+  case 'rainha':
+    console.log('A rainha movimenta na diagonal, horizontal e vertical')
+    break;
+
+  case 'bispo':
+    console.log('O bispo movimenta na diagonal')
+    break;
+
+  case 'rei':
+    console.log('O rei movimenta uma casa independente da direção')
+    break;
+
+  default:
+    break;
+
 }
