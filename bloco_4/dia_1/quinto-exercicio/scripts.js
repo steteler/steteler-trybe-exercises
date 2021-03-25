3f99e5b84f90162b83b76b65f374ba7fda3ce8e3
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-25 12:53:11 -0300

    Terceira parte terminado

--- bloco_4/dia_1/quinto-exercicio/scripts.js
+++ bloco_4/dia_1/quinto-exercicio/scripts.js
@@ -1,8 +1,12 @@
 let a = 10;
 let b = 2;
+let c = 1;
 let maior;
 
 console.log('Adição:', a + b, '\nSubtração:', a - b, '\nMultiplicação:', a * b, '\nDivisão:', a / b, '\nMódulo:', a % b);
+
+
+
 
 if (a > b) {
   maior = a;
@@ -12,4 +16,25 @@ else {
   maior = b;
   console.log('\nO valor', b, 'é maior que o valor', a)
 }
+
+
+
+a = 7
+b = 7
+c = 6
+
+if (a > b && a > c) {
+  console.log('teste');
+}
+else if (b > a && b > c) {
+  console.log('teste2');
+}
+else if (c > a && c > b) {
+  console.log('teste3');
+}
+else {
+  console.log('Amigão, tem 2 números iguais ai bicho, pode n');
+}
+
+
 
