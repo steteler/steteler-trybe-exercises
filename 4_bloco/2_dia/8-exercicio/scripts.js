0f1bb21aa4a6043c823d571dccfc352329ec2e55
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-26 13:21:14 -0300

    Atualização dos exercícios

--- 4_bloco/2_dia/8-exercicio/scripts.js
+++ 4_bloco/2_dia/8-exercicio/scripts.js
@@ -2,10 +2,93 @@
 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total=0;
+let average=0;
 
-for (let index=0; index < numbers.length; index += 1) {
+/*
+
+ for (let index=0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
 
+*/
+
 
 // Parte 2
+
+/*
+
+for (let index=0; index < numbers.length; index += 1) {
+  total += numbers[index];
+}
+
+console.log('A soma de todos os números é:', total);
+
+*/
+
+
+// Parte 3
+
+/*
+
+for (let index=0; index < numbers.length; index += 1) {
+  average += numbers[index];
+}
+average = (average/numbers.length);
+
+console.log(average);
+
+*/
+
+
+// Parte 4
+
+/*
+
+for (let index=0; index < numbers.length; index += 1) {
+  average += numbers[index];
+}
+average = (average/numbers.length);
+
+if (average > 20) {
+  console.log('A média é maior que 20, a média foi de:', average);
+}
+else {
+  console.log('A média é menor que 20, a média foi de:', average);
+}
+
+*/
+
+
+// Parte 5
+
+/*
+
+let higherNumber=0;
+for (let index=0; index < numbers.length; index += 1) {
+  if (higherNumber < numbers[index]) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log('O maior número é:', higherNumber);
+
+*/
+
+
+// Parte 6
+
+
+let oddNumber=0;
+for (let index=0; index < numbers.length; index += 1) {
+  if ((numbers[index] % 2) === 1) {
+    oddNumber += 1;
+  }
+}
+
+console.log('Foram encontrados', oddNumber, 'números impares');
+
+
+// Parte 7
+
+
