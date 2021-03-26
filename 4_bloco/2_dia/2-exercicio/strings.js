06b8e202763f17205336aef7ac8a3880be2f5a14
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-26 00:25:59 -0300

    Atualizado

--- 4_bloco/2_dia/2-exercicio/strings.js
+++ 4_bloco/2_dia/2-exercicio/strings.js
@@ -1,7 +1,11 @@
 let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let indexTasks;
 
 tasksList.pop();
 console.log(tasksList);
 
 tasksList.shift();
 console.log(tasksList);
+
+indexTasks = tasksList.indexOf('Reunião') 
+console.log(indexTasks);
