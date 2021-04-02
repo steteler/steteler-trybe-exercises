46422aedd68ee909ad87765e2c61a93037569d9c
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-02 18:30:11 -0300

    atualizado 11-exercicio 4_dia 4_bloco

--- 4_bloco/4_dia/2-parte/11-exercicio/scripts.js
+++ 4_bloco/4_dia/2-parte/11-exercicio/scripts.js
@@ -1,10 +1,10 @@
-console.log('O maior valor é: ' + highestIndexValue([2, 3, 6, 7, 10, 1]));
+console.log('O maior índice é: ' + highestIndexValue([2, 3, 6, 7, 10, 1]));
 
 function highestIndexValue(values) {
   let highestValue=0;
   for (let attributes in values) {
     if (highestValue < values[attributes]) {
-      highestValue = values[attributes];
+      highestValue = attributes;
     }
   }
   return highestValue;
