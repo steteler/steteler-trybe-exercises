d5142431ac12e0f9866e9e464f3174ec8df0f005
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-05 15:38:02 -0300

    adicionado novo bloco

---
+++ 5_bloco/1_dia/1-exercicio/scripts.js
@@ -0,0 +1,12 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = "Jim Carrey The Cable Man";
+
+var secondParagraph = document.getElementById("second-paragraph");
+
+secondParagraph.style.color = "blue";
+
+var subTitle = document.getElementById("subtitle");
+
+subTitle.style.fontSize = '24px';
