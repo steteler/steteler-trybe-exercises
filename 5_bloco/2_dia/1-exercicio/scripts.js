ddbf3b67e2f0838df34267485fcb1b57db9aba7f
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-07 12:28:29 -0300

    arrumado 1-exercicio 2_dia 5_bloco

--- 5_bloco/2_dia/1-exercicio/scripts.js
+++ 5_bloco/2_dia/1-exercicio/scripts.js
@@ -1,16 +1,18 @@
+//Exercicio 1
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+//Exercicio 2
 elementoOndeVoceEsta.parentElement.style.color = 'red';
-
+//Exercicio 3
 const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
 primeiroFilhoDoFilho.innerText = 'Teste';
-
+//Exercicio 4
 const pai = document.getElementById('pai');
-console.log(pai.firstElementChild);
-
-console.log(elementoOndeVoceEsta.previousElementSibling);
-
+pai.firstElementChild;
+//Exercicio 5
+elementoOndeVoceEsta.previousElementSibling;
+//Exercicio 6
 console.log(elementoOndeVoceEsta.nextSibling);
-
-console.log(elementoOndeVoceEsta.nextElementSibling);
-
-console.log(pai.childNodes[5]);
+//Exercicio 7
+elementoOndeVoceEsta.nextElementSibling;
+//Exercicio 8
+pai.childNodes[5];
