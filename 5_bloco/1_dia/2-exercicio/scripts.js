bd64f5580993b5d3b53409400e20b4e786bdd0f9
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-09 16:42:27 -0300

    atualizado 2 e 3 exercicio 1_dia 5_bloco

---
+++ 5_bloco/1_dia/2-exercicio/scripts.js
@@ -0,0 +1,6 @@
+const paragraph = document.getElementsByClassName('big');
+paragraph[0].style.color = 'red'
+
+const secondParagraph = document.getElementsByClassName('small');
+
+const subtitle = document.getElementsByTagName('h4');
