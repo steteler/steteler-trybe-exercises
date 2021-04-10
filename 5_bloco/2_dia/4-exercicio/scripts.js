7e3fa8c62b86ce94b83a6a83cbcdee5f15231231
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-10 01:37:38 -0300

    arrumado de novo 4-exercicio 2_dia 5_bloco

--- 5_bloco/2_dia/4-exercicio/scripts.js
+++ 5_bloco/2_dia/4-exercicio/scripts.js
@@ -4,33 +4,33 @@ h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 
 document.body.appendChild(h1);
 // Exercicio 2
-const div = document.createElement('div');
-div.className = 'main-content';
-document.body.appendChild(div);
+const divMain = document.createElement('div');
+divMain.className = 'main-content';
+document.body.appendChild(divMain);
 // Exercicio 3
-const div2 = document.createElement('div');
-div2.className = 'center-content';
-div.appendChild(div2);
+const divCenter = document.createElement('div');
+divCenter.className = 'center-content';
+divMain.appendChild(divCenter);
 // Exercicio 4
 const p = document.createElement('p');
 p.innerText = 'Algum texto';
-div.appendChild(p);
+divCenter.appendChild(p);
 // Exercicio 5
-const div3 = document.createElement('div');
-div3.className = 'left-content';
-div.appendChild(div3);
+const divLeftContent = document.createElement('div');
+divLeftContent.className = 'left-content';
+divMain.appendChild(divLeftContent);
 // Exercicio 6
-const div4 = document.createElement('div');
-div4.className = 'right-content';
-div.appendChild(div4);
+const divRightContent = document.createElement('div');
+divRightContent.className = 'right-content';
+divMain.appendChild(divRightContent);
 // Exercicio 7
 const img = document.createElement('img');
 img.src = 'https://picsum.photos/200';
 img.className = 'small-Image';
-div.appendChild(img);
+divLeftContent.appendChild(img);
 // Exercicio 8
 const ul = document.createElement('ul');
-div4.appendChild(ul);
+divRightContent.appendChild(ul);
 
 const arrayListItem = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez'];
 for (let index in arrayListItem) {
@@ -41,25 +41,26 @@ for (let index in arrayListItem) {
 // Exercicio 9
 for (let index = 0; index < 3; index += 1) {
   const h3 = document.createElement('h3');
+  h3.innerHTML = 'Show ' + index;
 
   // PARTE 2, Exercicio 2
   h3.className = 'description'
 
-  div4.appendChild(h3);
+  divMain.appendChild(h3);
 }
 
 // PARTE 2, Exercicio 1
 h1.className = 'title';
 
 // PARTE 2, Exercicio 3
-div.removeChild(div3);
+divMain.removeChild(divLeftContent);
 
 // PARTE 2 Exercicio 4
-div4.style.margin = 'auto';
+divRightContent.style.margin = 'auto';
 
 // PARTE 2 Exercicio 5
-div2.parentElement.style.backgroundColor = 'green';
+divCenter.parentElement.style.backgroundColor = 'green';
 
 // PARTE 2 Exercicio 6
-div.lastChild.remove();
-div.lastChild.remove();
+ul.lastChild.remove()
+ul.lastChild.remove()
