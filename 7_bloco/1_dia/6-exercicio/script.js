07c43adb28b5fc519c97c7607f2908b05922fd4c
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-30 15:55:27 -0300

    terminado 6-exercicio 1_dia 7_bloco

--- 7_bloco/1_dia/6-exercicio/script.js
+++ 7_bloco/1_dia/6-exercicio/script.js
@@ -2,25 +2,15 @@ const array = ["Android", "iOS", "Architecture", "Teach", "Run"]
 
 function buildSkillsPhrase (paramOne) {
   const fun1 = paramInner => (
-    `Tryber ${paramInner} aqui!
-
-    Tudo bem?`
+    `Tryber ${paramInner} aqui! Tudo bem?`
   )
 
-  let result = `${fun1(paramOne)}
-
-  Minhas cinco principais habilidades são:`
+  let result = `${fun1(paramOne)}\n\nMinhas cinco principais habilidades são:`
 
   array.forEach((skill, index) =>
-  result = `${result}
+  result = `${result}\n\n- ${skill}`)
 
-  - ${skill}`)
-
-  result = `
-{result}
-
-  #goTrybe
-  `
+  result = `${result}\n\n#goTrybe`
 
   return result
 }
