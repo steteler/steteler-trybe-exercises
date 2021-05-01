4088118864a600bd0e266e09bf380fd8bd1424f7
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-01 03:49:52 -0300

    atualizado 4-exercicio 1_dia 7_bloco

--- 7_bloco/1_dia/4-exercicio/script.js
+++ 7_bloco/1_dia/4-exercicio/script.js
@@ -1,11 +1,14 @@
 function longestWord(phrase) {
   let wordArray = phrase.split(' ');
-  let biggestLetter = '';
+  let longestWord = '';
 
   for(const index of wordArray) {
-    if (index.length > biggestLetter.length) { biggestLetter = index };
+    if (index.length > longestWord.length) { longestWord = index };
   }
-  return biggestLetter;
+  return longestWord;
+
+  //Também da pra fazer desse jeito abaixo.
+  //const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0]
 }
 
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
