4d6544824b33ef35de2a35d70e7bc4691fc39e1b
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-01 03:39:09 -0300

    atualizado 2-exercicio 1_dia 7_bloco

--- 7_bloco/1_dia/2-exercicio/script.js
+++ 7_bloco/1_dia/2-exercicio/script.js
@@ -1,5 +1,3 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const sortedArray = () => oddsAndEvens.sort((a, b) => a - b);
-
-console.log(`Os números ${sortedArray()} se encontram ordenados de forma crescente!`);
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
