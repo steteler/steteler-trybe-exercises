9708600d761f20b5227e32a7ad9b6f9a1cacab35
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-01 19:57:57 -0300

    terminado 1-parte 3-exercicio 2_dia 7_bloco

---
+++ 7_bloco/2_dia/3-exercicio/script.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addNewKeyValue = (objeto, key, keyValue) => {
+  objeto[key] = keyValue;
+}
+
+addNewKeyValue(lesson2, 'turno', 'manhã');
+
+console.table(lesson2);
