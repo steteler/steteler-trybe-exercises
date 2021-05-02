9cee6b3b0a651d03856fb070eea0f2b9a9bb4147
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-02 00:43:22 -0300

    arrumado erros apontados pelo eslint

--- 4_bloco/1_dia/2-exercicio/scripts.js
+++ 4_bloco/1_dia/2-exercicio/scripts.js
@@ -1,21 +1,25 @@
 let patientId = 50;
-let isEnrolled = true;
+const isEnrolled = true;
 const patientInfo = {
   firstName: 'Ana',
   lastName: 'Santos',
 };
 const patientEmail = 'ana@email.com';
 
-console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail, typeof patientAge);
+for () {
 
-patientId = '50'
+}
+
+patientId = '50';
 
 console.log(typeof patientId);
 
-let base = 5;
-let altura = 8;
-let area = base * altura;
-let perimetro = base * 4
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail, typeof patientAge);
+
+const base = 5;
+const altura = 8;
+const area = base * altura;
+const perimetro = base * 4;
 
 console.log(area);
-console.log(perimetro)
+console.log(perimetro);
