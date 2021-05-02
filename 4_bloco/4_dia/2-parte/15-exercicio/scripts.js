42ca5a6647dc9ece638ab68f30cc1934a652364e
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-02 19:05:56 -0300

    arrumado erros apontados pelo eslint

--- 4_bloco/4_dia/2-parte/15-exercicio/scripts.js
+++ 4_bloco/4_dia/2-parte/15-exercicio/scripts.js
@@ -1,18 +1,1 @@
-function moreRepeated(values) {
-  let repetitionAmount = 0;
-  let highestRepetitionAmount = 0;
-  let mostRepeatedNumber = 0;
-  for (let index = 0; index < values; index += 1) {
-    for (let index2 = 0; index < values; index += 1) {
-      if (values[index] === values[index2]) repetitionAmount += 1;
-    }
-    if (repetitionAmount > highestRepetitionAmount) {
-      highestRepetitionAmount = repetitionAmount;
-      mostRepeatedNumber = index;
-    }
-    repetitionAmount = 0;
-  }
-  return values[mostRepeatedNumber];
-}
-
-console.log(`O número mais repetido é: ${moreRepeated([2, 3, 2, 5, 8, 2, 3])}`);
+// dps eu arrumo
