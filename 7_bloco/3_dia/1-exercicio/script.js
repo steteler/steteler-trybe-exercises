be9ad06f01091476a5a6a853e44a462a27e04373
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-07 18:28:54 -0300

    adicionado comentários

--- 7_bloco/3_dia/1-exercicio/script.js
+++ 7_bloco/3_dia/1-exercicio/script.js
@@ -8,7 +8,11 @@ function sum(a, b) {
 }
 
 assert.strictEqual(typeof sum, 'function');
+// Exercicio 1
 assert.strictEqual(sum(4, 5), 9);
+// Exercicio 2
 assert.strictEqual(sum(0, 0), 0);
+// Exercicio 3
 assert.throws(() => { sum(4, '5'); });
+// Exercicio 4
 assert.throws(() => { sum(4, '5'); }, /^Error: parameters must be numbers$/);
