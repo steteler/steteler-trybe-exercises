9a8ddbf1657fd8fe722e1b473cfe2b4a026bcc44
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-18 21:55:27 -0300

    modificado descrições nos testes

--- 10_bloco/1_dia/1-exercicio/sum.test.js
+++ 10_bloco/1_dia/1-exercicio/sum.test.js
@@ -2,21 +2,21 @@
 const sum = require('./sum');
 
 describe('Função sum()', () => {
-  it('1 - Se o retorno de sum(4, 5) é 9', () => {
+  it('1 - Verifica se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
 
-  it('2 - Se o retorno de sum(0, 0) é 0', () => {
+  it('2 - Verifica se o retorno de sum(0, 0) é 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
 
-  it('3 - Se retorna erro ao receber string no parâmetros', () => {
+  it('3 - Verifica se retorna erro ao receber string no parâmetros', () => {
     expect(() => {
       sum(4, '5');
     }).toThrow();
   });
 
-  it('4 - Se retorna o erro "parameters must be numbers"', () => {
+  it('4 - Verifica se retorna o erro "parameters must be numbers"', () => {
     expect(() => {
       sum(4, '5');
     }).toThrow(/parameters must be numbers/);
