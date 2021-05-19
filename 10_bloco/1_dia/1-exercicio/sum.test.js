c1e35c80d0dd70c37adf8fa20ae881dc110afdae
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-18 21:37:24 -0300

    terminado 1-exercicio 1_dia 10_bloco

--- 10_bloco/1_dia/1-exercicio/sum.test.js
+++ 10_bloco/1_dia/1-exercicio/sum.test.js
@@ -1,5 +1,24 @@
-const sum = (a, b) => a + b;
+/* eslint-disable max-lines-per-function */
+const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+describe('Função sum()', () => {
+  it('1 - Se o retorno de sum(4, 5) é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('2 - Se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('3 - Se retorna erro ao receber string no parâmetros', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  it('4 - Se retorna o erro "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow(/parameters must be numbers/);
+  });
 });
