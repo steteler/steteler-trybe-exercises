ced5da5dd8a17bc67531da1c157d312580ce85cf
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-05-20 18:19:01 -0300

    erros eslint arrumado + código refatorado

--- 8_bloco/1_dia/5-exercicio/script.js
+++ 8_bloco/1_dia/5-exercicio/script.js
@@ -1,21 +1,12 @@
+/* eslint-disable no-return-assign */
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const checkAnswer = (rightAnswers, studentAnswers, callback) => {
-  let total = 0;
-  // eslint-disable-next-line guard-for-in
-  for (const answer in studentAnswers) {
-    total += callback(rightAnswers[answer], studentAnswers[answer]);
-  }
-  return total;
-};
+const checkAnswer = (rightAnswers, studentAnswers, callback) =>
+  studentAnswers.reduce((accumulator, _, index) => accumulator += callback(rightAnswers[index], studentAnswers[index]), 0);
 
 console.log(checkAnswer(RIGHT_ANSWERS, STUDENT_ANSWERS, (rAnswersIndex, sAnswersIndex) => {
-  if (sAnswersIndex === rAnswersIndex) {
-    return 1;
-  } 
-  if (sAnswersIndex === 'N.A') {
-    return 0;
-  }
+  if (sAnswersIndex === rAnswersIndex) return 1; 
+  if (sAnswersIndex === 'N.A') return 0;
   return 0.5;
 }));
