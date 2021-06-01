761f68f58d9b285df0e7f1c11e40799e0f89f4af
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-06-01 16:14:38 -0300

    refatorado 2_dia 4_bloco

--- 1-fundamentos/4_bloco/2_dia/10-exercicio/script.js
+++ 1-fundamentos/4_bloco/2_dia/10-exercicio/script.js
@@ -1,5 +1,10 @@
+let average = 0;
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
+  average += numbers[index];
 }
+
+average /= numbers.length;
+
+console.log(average);
--- 1-fundamentos/4_bloco/2_dia/9-exercicio/script.js
+++ 1-fundamentos/4_bloco/2_dia/9-exercicio/script.js
@@ -1,5 +1,8 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
+  total += numbers[index];
 }
+
+console.log('A soma de todos os números é:', total);
