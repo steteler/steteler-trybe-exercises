c24111c796457fffa14c50cb3e86628c3cb7b282
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-06-25 22:10:54 -0300

    terminado 5-task 3_day 10_block

---
+++ 1-fundamentos/10_block/3_day/5-task/script.spec.js
@@ -0,0 +1,20 @@
+const math = require('./script');
+
+test('Mock da função subtrair de modo que possa restaurar sua implementação original', () => {
+  const mockSubtrair = jest.spyOn(math, 'subtrair');
+  mockSubtrair.mockImplementation((a, b) => a * b);
+  mockSubtrair.mockReturnValue(20);
+
+  expect(mockSubtrair(10, 2)).toBe(20);
+  expect(mockSubtrair).toHaveBeenCalled();
+  expect(mockSubtrair).toHaveBeenCalledTimes(1);
+  expect(mockSubtrair).toHaveBeenCalledWith(10, 2);
+
+  mockSubtrair.mockRestore();
+  mockSubtrair.mockReturnValue(8);
+
+  expect(mockSubtrair(10, 2)).toBe(8);
+  expect(mockSubtrair).toHaveBeenCalled();
+  expect(mockSubtrair).toHaveBeenCalledTimes(1);
+  expect(mockSubtrair).toHaveBeenCalledWith(10, 2);
+});
