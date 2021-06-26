baa0f13082a224d26facdf67f2ca060e8fa555ff
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-06-25 23:46:46 -0300

    terminado 6-task 3_day 10_block

---
+++ 1-fundamentos/10_block/3_day/6-task/script.spec.js
@@ -0,0 +1,9 @@
+const service = require('./script');
+
+test('testando se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
+  service.randomNumber = jest.fn().mockReturnValue(10);
+
+  expect(service.randomNumber()).toBe(10);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+});
