bebef658cd5e70b71ad66dd1081bf84a2277fd71
kikutii <joaovictorkikuti@gmail.com>
2021-10-14 16:58:20 -0300

    terminado 1-task 1_day

--- 2-front-end/11_block/1_day/1-task/src/App.js
+++ 2-front-end/11_block/1_day/1-task/src/App.js
@@ -1,12 +1,39 @@
 import { Component } from 'react';
-import Data from './data';
-import Pokemons from './Pokemons';
-
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      contador1: 0,
+      contador2: 0,
+      contador3: 0,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(state) {
+    this.setState((previousState, _props) => (
+      {
+        [state]: previousState[state] + 1
+      }
+    ), () => { console.log(`O botão: ${state} está com a cor ${this.changeButtonColor(this.state[state])}`) })
+  }
+
+  changeButtonColor(state) {
+    return state % 2 ? 'white' : 'green';
+  }
+
   render() {
+    const { contador1, contador2, contador3 } = this.state;
+
     return (
-      <Pokemons pokemons={Data}/>
-    );
+      <div>
+        <button onClick={() => this.handleClick('contador1')} style={{ backgroundColor: this.changeButtonColor(contador1) }}>{contador1}</button>
+        <button onClick={() => this.handleClick('contador2')} style={{ backgroundColor: this.changeButtonColor(contador2) }}>{contador2}</button>
+        <button onClick={() => this.handleClick('contador3')} style={{ backgroundColor: this.changeButtonColor(contador3) }}>{contador3}</button>
+      </div>
+    )
   }
 }
 
--- 2-front-end/11_block/1_day/1-task/src/index.js
+++ 2-front-end/11_block/1_day/1-task/src/index.js
@@ -1,4 +1,3 @@
-import './Pokemons.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
