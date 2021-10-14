9a7dd447c2273c7d47ef5794f21e19cda99ff243
kikutii <joaovictorkikuti@gmail.com>
2021-10-14 18:32:36 -0300

    terminado 1-task 2_day

---
+++ 2-front-end/11_block/2_day/1-task/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import Data from './data';
+import Pokemons from './Pokemons';
+
+class App extends Component {
+  render() {
+    return (
+      <Pokemons pokemons={Data}/>
+    );
+  }
+}
+
+export default App;
