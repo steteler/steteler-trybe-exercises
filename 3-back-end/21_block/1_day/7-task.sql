a2f2c7904076a4beb514169df3eeff64ef26633b
kikutii <joaovictorkikuti@gmail.com>
2022-08-21 16:53:22 -0300

    feat: 7-task/1_day/21_block finished

---
+++ 3-back-end/21_block/1_day/7-task.sql
@@ -0,0 +1,15 @@
+USE sakila;
+
+-- requirement 1 // requisito 1
+SELECT rating, AVG(length) AS 'average_duration'
+FROM film
+GROUP BY rating
+HAVING average_duration BETWEEN 115.0 AND 121.50
+ORDER BY average_duration DESC;
+
+-- requirement 2 // requisito 2
+SELECT rating, SUM(replacement_cost) AS 'total_replacement_cost'
+FROM film
+GROUP by rating
+HAVING total_replacement_cost > 3950.50
+ORDER BY total_replacement_cost;
