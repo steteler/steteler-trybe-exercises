236fdc524de2902e140694df0f82b86164681f16
kikutii <joaovictorkikuti@gmail.com>
2022-08-21 09:11:41 -0300

    feat: 2-task/1_day/21_block add new exercise

--- 3-back-end/21_block/1_day/2-task.sql
+++ 3-back-end/21_block/1_day/2-task.sql
@@ -1,8 +1,22 @@
 USE sakila;
 
+-- requirement 1 // requisito 1
 SELECT 
 	film_id,
     title,
 	IF(title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme')
 AS 'conheço o filme?'
+FROM film;
+
+-- requirement 2 // requisito 2
+SELECT 
+	title,
+    rating,
+    CASE 
+		WHEN rating = 'G' THEN 'Livre para todos'
+        WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+        WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+        WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+        ELSE 'Proibido para menores de idade'
+	END AS 'público-alvo'
 FROM film;
