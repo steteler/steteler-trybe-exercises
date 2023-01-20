bc2803007e2cf9566ec9ba690819235250ece2c3
kikuti <joaovictorkikuti@gmail.com>
2023-01-20 15:12:07 -0300

    feat: add geyById in book controller

--- 3-back-end/6_section/1_day/src/controllers/book.controller.js
+++ 3-back-end/6_section/1_day/src/controllers/book.controller.js
@@ -6,6 +6,19 @@ async function getAll(_req, res) {
   res.status(200).json({ message: books });
 }
 
+async function getById(req, res) {
+  const { id } = req.params;
+
+  const book = await service.book.getById(id);
+
+  if (!book) {
+    res.status(404).json({ message: 'Book not found' });
+  }
+
+  res.status(200).json({ message: book });
+}
+
 module.exports = {
   getAll,
+  getById,
 };
