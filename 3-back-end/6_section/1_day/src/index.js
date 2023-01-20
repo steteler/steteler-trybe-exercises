d0d9739ad72e9356bdb3949e55f39c934903b51e
kikuti <joaovictorkikuti@gmail.com>
2023-01-20 13:04:33 -0300

    feat: add bookRoutes and refactor with eslint

--- 3-back-end/6_section/1_day/src/index.js
+++ 3-back-end/6_section/1_day/src/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
+
 const express = require('express');
+const { bookRoutes } = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
 app.use(express.json());
+app.use('/books', bookRoutes);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
