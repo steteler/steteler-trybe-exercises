380daf3e39d8b5350a14b4bcbddd3ae7e4e9b54c
kikuti <joaovictorkikuti@gmail.com>
2023-01-20 15:52:54 -0300

    feat: add updateBook in the book controller to return if the book was updated

--- 3-back-end/6_section/1_day/src/controllers/book.controller.js
+++ 3-back-end/6_section/1_day/src/controllers/book.controller.js
@@ -12,10 +12,10 @@ async function getById(req, res) {
   const book = await service.book.getById(id);
 
   if (!book) {
-    res.status(404).json({ message: 'Book not found' });
+    return res.status(404).json({ message: 'Book not found' });
   }
 
-  res.status(200).json(book);
+  return res.status(200).json(book);
 }
 
 async function createNewBook(req, res) {
@@ -26,8 +26,22 @@ async function createNewBook(req, res) {
   res.status(201).json(newBook);
 }
 
+async function updateBook(req, res) {
+  const { id } = req.params;
+  const { title, author, pageQuantity } = req.body;
+
+  const updatedBook = await service.book.updateBook(id, { title, author, pageQuantity });
+
+  if (!updatedBook) {
+    return res.status(404).json({ message: 'Book not found!' });
+  }
+
+  return res.status(201).json({ message: 'Book updated' });
+}
+
 module.exports = {
   getAll,
   getById,
   createNewBook,
+  updateBook,
 };
