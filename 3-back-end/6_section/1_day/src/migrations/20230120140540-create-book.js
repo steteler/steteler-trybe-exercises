7ecc497736d896cac6fe69c681d6be568d70f0a7
kikuti <joaovictorkikuti@gmail.com>
2023-01-20 11:31:21 -0300

    feat: created initial sequelize migration to create table books

---
+++ 3-back-end/6_section/1_day/src/migrations/20230120140540-create-book.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('books', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('books')
+  }
+};
