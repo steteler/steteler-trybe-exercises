f3b58f3c5159305853cce026846974b774ce2206
kikuti <joaovictorkikuti@gmail.com>
2023-01-20 15:52:16 -0300

    refactor: change the import method

--- 3-back-end/6_section/1_day/src/index.js
+++ 3-back-end/6_section/1_day/src/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const { bookRoutes } = require('./routes/index');
+const routes = require('./routes/index');
 
 const app = express();
 const { PORT } = process.env;
 
 app.use(express.json());
-app.use('/books', bookRoutes);
+app.use('/books', routes.book);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
