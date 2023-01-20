1714bd18d89981fe55340703e3df028785853702
kikuti <joaovictorkikuti@gmail.com>
2023-01-20 15:12:32 -0300

    feat: add the book id route in the book routes

--- 3-back-end/6_section/1_day/src/routes/book.routes.js
+++ 3-back-end/6_section/1_day/src/routes/book.routes.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/index');
 const bookRouter = express.Router();
 
 bookRouter.get('/', controller.book.getAll);
+bookRouter.get('/:id', controller.book.getById);
 
 module.exports = bookRouter;
