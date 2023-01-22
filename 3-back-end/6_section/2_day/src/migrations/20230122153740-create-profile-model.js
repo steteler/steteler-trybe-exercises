6abd6dd7e8c691a68d8c32874eb7ab1e48eb9ba3
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 17:01:24 -0300

    feat: add migration to create table profiles in the db

---
+++ 3-back-end/6_section/2_day/src/migrations/20230122153740-create-profile-model.js
@@ -0,0 +1,38 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      last_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      account_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'accounts',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('profiles');
+  },
+};
