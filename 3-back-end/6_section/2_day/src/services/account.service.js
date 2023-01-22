5901cd8e198c2ca0e1a494063a463f4ae45d2dcf
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 20:49:29 -0300

    feat: add lazyLoading option in account service

--- 3-back-end/6_section/2_day/src/services/account.service.js
+++ 3-back-end/6_section/2_day/src/services/account.service.js
@@ -1,6 +1,14 @@
 const { Account, Profile } = require('../models/index');
 
-async function getById(id) {
+async function getById(id, lazyLoading) {
+  // lazy loading
+  if (lazyLoading === 'true') {
+    const accountById = await Account.findByPk(id);
+
+    return { ...accountById.dataValues };
+  }
+
+  // eager loading
   const accountById = await Account.findByPk(
     id,
     {
