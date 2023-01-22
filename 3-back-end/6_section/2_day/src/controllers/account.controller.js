087a30627f09d671f5b0aa99c16b754a38b30fb4
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 20:48:11 -0300

    feat: add in account controller lazyLoading query and send to account services

--- 3-back-end/6_section/2_day/src/controllers/account.controller.js
+++ 3-back-end/6_section/2_day/src/controllers/account.controller.js
@@ -1,8 +1,10 @@
 const services = require('../services/index');
 
 async function getById(req, res) {
+  const { lazyLoading } = req.query;
   const { id } = req.params;
-  const accountById = await services.account.getById(id);
+
+  const accountById = await services.account.getById(id, lazyLoading);
   res.status(200).json(accountById);
 }
 
