1e505ad9138b5b1bf9169291cffe9b2a0f265bb0
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 20:47:35 -0300

    feat: add lazy loading route

--- 3-back-end/6_section/2_day/src/app.js
+++ 3-back-end/6_section/2_day/src/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+// using eager loading
 app.use('/accounts', routes.account);
+
+// using lazy loading
+app.use('/accounts-v2', routes.accountV2);
 
 module.exports = app;
