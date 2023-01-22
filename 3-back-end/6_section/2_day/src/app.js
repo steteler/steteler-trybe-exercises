b1476e793d12c3df2a9de71b7bd1b7dc7581d7cd
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 20:19:08 -0300

    feat: add accounts route in app js

--- 3-back-end/6_section/2_day/src/app.js
+++ 3-back-end/6_section/2_day/src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/accounts', routes.account);
 
 module.exports = app;
