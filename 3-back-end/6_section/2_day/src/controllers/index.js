ea8e6d805eea5c7d29ccaf540b42af09f007762b
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 21:11:48 -0300

    feat: add comment to the barrels

--- 3-back-end/6_section/2_day/src/controllers/index.js
+++ 3-back-end/6_section/2_day/src/controllers/index.js
@@ -1,5 +1,7 @@
 const account = require('./account.controller');
+const comment = require('./comment.controller');
 
 module.exports = {
   account,
+  comment,
 };
--- 3-back-end/6_section/2_day/src/services/index.js
+++ 3-back-end/6_section/2_day/src/services/index.js
@@ -1,5 +1,7 @@
 const account = require('./account.service');
+const comment = require('./comment.service');
 
 module.exports = {
   account,
+  comment,
 };
