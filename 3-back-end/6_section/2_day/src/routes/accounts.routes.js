68fbf86ca741f98f90774331fde0bfae476d7fe2
kikuti <joaovictorkikuti@gmail.com>
2023-01-22 21:12:10 -0300

    feat: add new comments route

--- 3-back-end/6_section/2_day/src/routes/accounts.routes.js
+++ 3-back-end/6_section/2_day/src/routes/accounts.routes.js
@@ -4,5 +4,6 @@ const controllers = require('../controllers/index');
 const accountRouter = express.Router();
 
 accountRouter.get('/:id', controllers.account.getById);
+accountRouter.get('/:id/comments', controllers.comment.getCommentsByAccountId);
 
 module.exports = accountRouter;
