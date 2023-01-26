23ade3bb3541fb718e632f07d59444ba58334651
kikuti <joaovictorkikuti@gmail.com>
2023-01-26 14:45:54 -0300

    feat: add patient route in app js

--- 3-back-end/6_section/3_day/src/app.js
+++ 3-back-end/6_section/3_day/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
-
 app.use(express.json());
+
+app.use('/all', routes.patientRouter);
 
 module.exports = app;
